c4f1dfc415b04e18115f55483468a88686e6e70c
Waire <waire.tega@gmail.com>
2021-04-13 15:00:47 +0100

    update

---
+++ intelligent.js
@@ -0,0 +1,12 @@
+// change the first letter of every word in an array
+// let array = ["trust","deny"];
+// function cases(){
+//     let changed = array.map(myFunction);
+//     function myFunction(value) {
+//         let y = value[0]
+//         let z = value[0].toUpperCase();
+//         return value.replace(y,z);
+//     }
+//     return changed;
+// }
+// console.log(cases());
